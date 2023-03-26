827ea090e75d93d56b50f3efcf1be2b3d9ef47b1
wildan1311 <wmaaf1311@gmail.com>
2023-03-26 14:45:47 +0700

    merubah sedikit tampilan views dan menambahkan gambar

--- resources/views/testing.blade.php
+++ resources/views/testing.blade.php
@@ -1,52 +1,113 @@
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    @vite(['resources/css/app.css','resources/js/app.js'])
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css"
+        integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
+
 <body>
-    <nav class="bg-red-500">
-        <div class="container mx-auto py-5 flex justify-between">
-            <div class="bg-primary">
-                <h1>MealQ</h1>
+
+    <nav class="bg-white px-2 sm:px-4 py-2.5 dark:bg-gray-900 w-full z-20 top-0 left-0 border-b border-gray-200">
+        <div class="container flex flex-wrap items-center justify-between mx-auto">
+            <a href="https://flowbite.com/" class="flex items-center">
+                <img src="https://flowbite.com/docs/images/logo.svg" class="h-6 mr-3 sm:h-9" alt="Flowbite Logo">
+                <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+            </a>
+            <div class="flex md:order-2">
+                <button type="button"
+                    class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Get
+                    started</button>
+                <button data-collapse-toggle="navbar-sticky" type="button"
+                    class="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                    aria-controls="navbar-sticky" aria-expanded="false">
+                    <span class="sr-only">Open main menu</span>
+                    <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd"
+                            d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                            clip-rule="evenodd"></path>
+                    </svg>
+                </button>
             </div>
-            <ul class="flex gap-3 justify-center">
-                <li class="rounded-lg  px-3 py-1 hover:bg-red-400">Home</li>
-                <li class="rounded-lg  px-3 py-1 hover:bg-red-400">About</li>
-                <li class="rounded-lg  px-3 py-1 hover:bg-red-400">App</li>
-                <li class="rounded-lg  px-3 py-1 hover:bg-red-400">Contact</li>
-            </ul>
+            <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
+                <ul
+                    class="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                    <li>
+                        <a href="#"
+                            class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white"
+                            aria-current="page">Home</a>
+                    </li>
+                    <li>
+                        <a href="#"
+                            class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</a>
+                    </li>
+                    <li>
+                        <a href="#"
+                            class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Services</a>
+                    </li>
+                    <li>
+                        <a href="#"
+                            class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
+                    </li>
+                </ul>
+            </div>
         </div>
     </nav>
 
+
     <section>
-        <div class="flex items-center justify-around mx-auto py-20 container">
-            <div class="">
-                <img src="{{asset("images/makan.png")}}" alt="BELUM ADA BOSS" class="w-full aspect-auto">
+        <div class="flex flex-col md:flex-row items-center justify-around mx-auto md:container h-screen">
+            <div class="w-full md:-3/5">
+                <img src="{{ asset('images/fotor_AI.png') }}" alt="BELUM ADA BOSS"
+                    class="w-full aspect-auto object-cover">
             </div>
-            <div class="w-2/5">
-                <p class="text-4xl font-bold my-3">Kantin lebih efisien <br>dengan MealQ</p>
-                <p class="my-5">Nikmati makanan enak tanpa harus merasa kesal menunggu</p>
-                <button class="rounded-full bg-red-200 px-5 py-2 hover:bg-red-500 items-center"><a href="" class="text-xl font-bold">Download <span>&#11166</span></a></button>
+            <div class="w-5/5 md:w-2/5 md:flex md:flex-col md:items-center md:justify-center">
+                <p class="text-4xl font-bold my-3 text-center">Kantin lebih efisien <br>dengan MealQ</p>
+                <p class="my-5 text-center">Nikmati makanan enak tanpa harus merasa kesal menunggu</p>
+                <button class="rounded-full bg-red-200 px-5 py-2 hover:bg-red-500 self-center w-full"><a href=""
+                        class="text-xl font-bold">Download <span>&#11166</span></a></button>
             </div>
         </div>
     </section>
 
-    <section class="bg-red-100 py-10">
+    <section class="bg-red-100 py-10 container">
         <div class=" flex flex-col items-center justify-center">
-            <p class="text-4xl font-bold my-3">About Us</p><br>
-            <p>Sebuah Aplikasi pemesanan makanan di Kantin PENS untuk mengurangi antrian yang panjang</p>
+            <p class="text-4xl font-bold my-3">Features</p><br>
         </div>
-        <div class="flex items-center justify-center mt-5">
-            <div class="card">
-                <div class="w-full">
-                    <i class="fas fa-chalkboard-teacher fa-5x"></i>
-                </div>
-            </div> 
+
+        <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+            <svg class="w-10 h-10 mb-2 text-gray-500 dark:text-gray-400" aria-hidden="true" fill="currentColor"
+                viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd"
+                    d="M5 5a3 3 0 015-2.236A3 3 0 0114.83 6H16a2 2 0 110 4h-5V9a1 1 0 10-2 0v1H4a2 2 0 110-4h1.17C5.06 5.687 5 5.35 5 5zm4 1V5a1 1 0 10-1 1h1zm3 0a1 1 0 10-1-1v1h1z"
+                    clip-rule="evenodd"></path>
+                <path d="M9 11H3v5a2 2 0 002 2h4v-7zM11 18h4a2 2 0 002-2v-5h-6v7z"></path>
+            </svg>
+            <a href="#">
+                <h5 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">Need a help in
+                    Claim?</h5>
+            </a>
+            <p class="mb-3 font-normal text-gray-500 dark:text-gray-400">Go to this step by step guideline process on
+                how to certify for your weekly benefits:</p>
+            <a href="#" class="inline-flex items-center text-blue-600 hover:underline">
+                See our guideline
+                <svg class="w-5 h-5 ml-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                    <path
+                        d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z">
+                    </path>
+                    <path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z">
+                    </path>
+                </svg>
+            </a>
         </div>
+
     </section>
 </body>
+
 </html>
